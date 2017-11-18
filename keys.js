f0f2d451d0bb79d84a0c79214373f80bbd77f758
michellechristina <michelle@thisisbam.com>
2017-11-18 17:09:20 -0500

    twitter added

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'kanWg3WXQxjjd75sCzIiu5t8O',
+  consumer_secret: 'siuJDiG7TcBrJ6Ooayc0w4FujREIYDEgfkQWUcgLDcp1KtDvLR',
+  access_token_key: '932001639027929090-KSdZiriL4BCfrgRe8g6G7e6MLFL7gb9',
+  access_token_secret: 'fHY6agp3n8JZCR9Mx8s4YbUNLObqNHgADpfxUot7twF4m',
 }
 
 module.exports = twitterKeys;
